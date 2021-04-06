4ccb3538c12b2ee19b8971f19f4477c2fcaf74c7
KaikOliveira <kaik.oliveiraa76@gmail.com>
2021-04-06 09:17:07 -0300

    addEditorConfigAndEslint

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-      <Text>Hello Wold</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: React.FC = () => (
+  <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+    <Text>Hello Wold</Text>
+    <StatusBar style="auto" />
+  </View>
+);
+
+export default App;
